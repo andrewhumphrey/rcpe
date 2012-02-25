35934389ef5848ec69636b6e859a7bd288f4aead
Andrew Humphrey <ahumphrey@classifiedadventures.com>
2012-02-25 22:56:32 +1100

    initial rails new run, with tweaks for mysql and capistrano

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rcpe::Application.config.secret_token = 'a8dfe3f16a0c8b384d0ee22aebb85cd2950bf2cba40f12dec4bdb9456a76d8d01ed18397ccf9c31dbc64162dc9c2cbe4b6e408117e6921d945288759735fe1cb'
