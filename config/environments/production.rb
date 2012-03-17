9d846ed85c0f98cae01b9d87f095a39dfa1b411f
Andrew Humphrey <ahumphrey@classifiedadventures.com>
2012-03-17 22:58:50 +1100

    Stop the moaning please

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rcpe::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  ::ActiveSupport::Deprecation.silenced = true
 
   # Code is not reloaded between requests
   config.cache_classes = true
